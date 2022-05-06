f1d880b136f53610a0a4831147a62b9cbc29c0b2
Devon Ankar <devon.ankar@gmail.com>
2022-05-06 12:39:53 -0400

    Reorder

--- title_generator.py
+++ title_generator.py
@@ -6,25 +6,6 @@ import spacy
 get_nlp_engine = spacy.load('en_core_web_sm')
 
 
-class BookTitleGenerator:
-    def __init__(self):
-        self._add_spaces = lambda data: [f' {i}' if i.isalnum() else i for i in data]
-        self._choose_random = lambda tokens, structures: [
-            random.choice(tokens[tokens.pos == pos].text.values) for pos in random.choice(structures)]
-
-        self._title_tokens = pd.read_csv('data/tokens_titles.csv', dtype=str)
-        self._subtitle_tokens = pd.read_csv('data/tokens_subtitles.csv', dtype=str)
-        self._title_structures = _read_structures_from_disk('titles')
-        self._subtitle_structures = _read_structures_from_disk('subtitles')
-
-    def get_reconstituted_title(self) -> str:
-        title_components = self._choose_random(self._title_tokens, self._title_structures)
-        subtitle_components = self._choose_random(self._subtitle_tokens, self._subtitle_structures)
-        reconstituted_title = ': '.join(tuple(map(lambda x: ''.join(self._add_spaces(x)).strip(), (
-            title_components, subtitle_components))))
-        return reconstituted_title
-
-
 def _split_subtitles() -> tuple[list, list]:
     just_titles = []
     subtitles = []
@@ -69,3 +50,22 @@ def _read_structures_from_disk(label: str) -> list:
     df = pd.read_csv(f'data/structures_{label}.csv', dtype=str)
     structures = [tuple(i for i in record if i and pd.notna(i)) for record in df.to_records(index=False)]
     return structures
+
+
+class BookTitleGenerator:
+    def __init__(self):
+        self._add_spaces = lambda data: [f' {i}' if i.isalnum() else i for i in data]
+        self._choose_random = lambda tokens, structures: [
+            random.choice(tokens[tokens.pos == pos].text.values) for pos in random.choice(structures)]
+
+        self._title_tokens = pd.read_csv('data/tokens_titles.csv', dtype=str)
+        self._subtitle_tokens = pd.read_csv('data/tokens_subtitles.csv', dtype=str)
+        self._title_structures = _read_structures_from_disk('titles')
+        self._subtitle_structures = _read_structures_from_disk('subtitles')
+
+    def get_reconstituted_title(self) -> str:
+        title_components = self._choose_random(self._title_tokens, self._title_structures)
+        subtitle_components = self._choose_random(self._subtitle_tokens, self._subtitle_structures)
+        reconstituted_title = ': '.join(tuple(map(lambda x: ''.join(self._add_spaces(x)).strip(), (
+            title_components, subtitle_components))))
+        return reconstituted_title
